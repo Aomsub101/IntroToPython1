1b3ab1aca731f6493e4f9f4feafc9a5949240532
Aomsub101 <lerdariyamaythee@gmail.com>
2024-09-12 20:12:53 +0700

    add note day 4

---
+++ work1_turtle.py
@@ -0,0 +1,15 @@
+# Making flower
+import turtle
+
+turtle.penup()
+turtle.backward(100)
+turtle.pendown()
+turtle.forward(200)
+turtle.left(110)
+turtle.forward(150)
+turtle.left(70)
+turtle.forward(100)
+turtle.left(70)
+turtle.forward(150)
+    
+turtle.mainloop()
