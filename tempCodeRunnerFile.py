2afa94041cf80f4d787581647c7a67ade7d498b3
Aomsub101 <lerdariyamaythee@gmail.com>
2024-09-16 19:56:04 +0700

    add note day 6

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+left_socks = int(input())
+right_socks = int(input())
+
+pairs = min(left_socks, right_socks)
+
+print(pairs)
---
+++ test.py
@@ -0,0 +1,6 @@
+number = int(input())
+
+if number % 7 == 0 and number % 11 != 0:
+    print("YES")
+else:
+    print("NO")
