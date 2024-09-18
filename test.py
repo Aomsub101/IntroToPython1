0bdf987c3435538dca294d9c37fb6dd23e8e78df
Aomsub101 <lerdariyamaythee@gmail.com>
2024-09-19 00:16:11 +0700

    add note day7 & organize the file

--- test.py
+++
@@ -1,7 +0,0 @@
-first_input = input()
-second_input = input()
-
-result_in_str = first_input + second_input
-result_in_number = int(first_input) + int(second_input)
-
-print(result_in_str, result_in_number)
